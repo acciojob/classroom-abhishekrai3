ad451b0c3c881289336f1f60329ba61013d7fe94
Abhishek Rai <62717502+abhishekrai3@users.noreply.github.com>
2024-04-04 14:34:56 +0530

    Add files via upload

---
+++ src/main/java/com/driver/Student.java
@@ -0,0 +1,48 @@
+package com.driver;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Student {
+
+    private String name;
+    private int age;
+    private double averageScore;
+
+    //@Bean
+    public Student(){
+
+    }
+   // @Bean
+    public Student(String name, int age, double averageScore) {
+        this.name = name;
+        this.age = age;
+        this.averageScore = averageScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
+}

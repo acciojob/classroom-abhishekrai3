a871b8720d0a71b405c5b1039a67aa62dbbe2d02
Abhishek Rai <62717502+abhishekrai3@users.noreply.github.com>
2024-04-04 16:23:02 +0530

    Update Teacher.java

--- src/main/java/com/driver/model/Teacher.java
+++ src/main/java/com/driver/model/Teacher.java
@@ -1,9 +1,9 @@
 package com.driver.model;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 
 public class Teacher {
 
